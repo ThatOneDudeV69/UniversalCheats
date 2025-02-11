185c2c2a8f42861efca3ed8212ad7edd44e86d63
ThatOneDudeV69 <sui999924@gmail.com>
2025-02-11 10:11:22 +0530

    Update pet.lua

--- pet.lua
+++ pet.lua
@@ -2,7 +2,7 @@
 local petsFolder = game:GetService("Players").LocalPlayer.petsFolder
 for _,v in petsFolder:GetChildren() do
    for _, pet in v:GetChildren() do
-      if pet.Name == "Cool Guy Larry" then
+      if pet.Name == "Flaming Hedgehog" then
         game:GetService("ReplicatedStorage"):WaitForChild("rEvents"):WaitForChild("equipPetEvent"):FireServer("equipPet", pet)
       end
    end
